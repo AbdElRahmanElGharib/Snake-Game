e491bf1c6ea62d8453372bd9db5436d06aa10982
AShahawy7 <abdoelshahawy45@gmail.com>
2023-08-06 07:36:52 +0300

    Added Fruit(refined)

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 import pygame, sys, random
 from pygame.math import Vector2
-
 class FRUIT:
     def __init__(self):
         self.x = random.randint(0,cell_number-1)
         self.y = random.randint(0,cell_number-1)
-        self.pos = Vector2(self.x,self.y)
-
-        
+        self.pos = Vector2(self.x,self.y)     
     def drawFruit(self):
         fruitRect = pygame.Rect(self.pos.x*cell_size,self.pos.y*cell_size,cell_size,cell_size)
-        pygame.draw.rect(screen,(126,166,114),fruitRect)
-            
+        pygame.draw.rect(screen,(126,166,114),fruitRect)           
+
 pygame.init()
 running = True
 cell_number = 20
 cell_size = 40
-
 screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock = pygame.time.Clock()
-
 fruit = FRUIT()
 
 while running:
@@ -31,5 +26,3 @@ while running:
     fruit.drawFruit()
     pygame.display.update()
     clock.tick(60)
-
-
