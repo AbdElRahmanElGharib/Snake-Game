f019b1cf90694f783d356f470fa6332b4fe8202c
AShahawy7 <abdoelshahawy45@gmail.com>
2023-08-08 03:14:19 +0300

    Added snake body

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pygame, sys, random
 from pygame.math import Vector2
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(7,10),Vector2(8,10),Vector2(9,10)]
+    def draw_snake(self):
+        for block in self.body:
+           block_rect = pygame.Rect(block.x*cell_size,block.y*cell_size,cell_size-2,cell_size-2)
+           pygame.draw.rect(screen,(123,11,223),block_rect)
 class FRUIT:
     def __init__(self):
         self.x = random.randint(0,cell_number-1)
@@ -7,7 +15,7 @@ class FRUIT:
         self.pos = Vector2(self.x,self.y)     
     def drawFruit(self):
         fruitRect = pygame.Rect(self.pos.x*cell_size,self.pos.y*cell_size,cell_size,cell_size)
-        pygame.draw.rect(screen,(126,166,114),fruitRect)           
+        pygame.draw.rect(screen,(255,20,20),fruitRect)           
 
 pygame.init()
 running = True
@@ -16,6 +24,7 @@ cell_size = 40
 screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock = pygame.time.Clock()
 fruit = FRUIT()
+snake = SNAKE()
 
 while running:
     for event in pygame.event.get():
@@ -24,5 +33,6 @@ while running:
             sys.exit()
     screen.fill((175,215,70))
     fruit.drawFruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
